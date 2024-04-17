1e42a5a01756ed6f281c7c01d0238c5126f8eec0
rudwnok <rudwnok123@naver.com>
2024-04-17 22:22:09 +0900

    sheet

--- src/components/home/category/Category.tsx
+++ src/components/home/category/Category.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import s from './category.module.scss';
 
 const Category = () => {
   return (
-    <div>
-      카테고리들
-      <br />
-      핫한 장소 - 
+    <div className={s.category}>
+      <ul>
+        <li>음식점</li>
+        <li>카페</li>
+        <li>술집</li>
+        <li>쇼핑/여행</li>
+        <li>가오픈/신상</li>
+        <li>배달주문</li>
+        <li>랭킹</li>
+        <li>모임/약속</li>
+      </ul>
+      <h3>핫한 장소</h3>
     </div>
   );
 };
