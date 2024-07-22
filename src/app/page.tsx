b6060884267c8548551bf911b3e7f98047a2ccab
rudwnok <rudwnok123@naver.com>
2024-07-22 23:32:24 +0900

    postgres 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import Header from '@/components/header/Header';
 import s from './main.module.scss';
-import Button from '@/components/button/Button';
+// import Button from '@/components/button/Button';
 import classNames from 'classnames';
 import BottomMenu from '@/components/bottomMenu/BottomMenu';
 import Location from '@/components/location/Location';
 import Sheet from '@/components/home/sheet/Sheet';
 import Map from '@/components/map/Map';
+import { Button } from 'snow-white-ui';
 
 export default function Home() {
   return (
     <div>
       <div className='content'>
         <Header theme='search' />
-        <Map/>
+        <Map />
         <Sheet />
+        <Button >111</Button>
       </div>
       <BottomMenu />
       <Location />
