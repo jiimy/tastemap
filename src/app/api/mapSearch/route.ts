8f51342a639861d00b6ac95b7fc7c51957dd7e41
rudwnok <rudwnok123@naver.com>
2024-08-07 20:42:10 +0900

    배포 실패 수정 테스트2

--- src/app/api/mapSearch/route.ts
+++ src/app/api/mapSearch/route.ts
@@ -1,16 +1,16 @@
-import apiInstance from "@/util/useInterceptor";
-import axios from "axios";
+// import apiInstance from "@/util/useInterceptor";
+// import axios from "axios";
 
-type mapType = {
-  search?: any;
-}
+// type mapType = {
+//   search?: any;
+// }
 
-// 지도 검색
-export async function GET({ search }: mapType) {
-  const res = await axios.get(`https://openapi.naver.com/v1/search/local.xml?query=${search}&display=10&start=1&sort=random`, {
-    headers: {
-      'X-Naver-Client-Id': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_ID}`, 'X-Naver-Client-Secret': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_SECRET}`,
-    }
-  })
-  return res;
-}
+// // 지도 검색
+// export async function GET({ search }: mapType) {
+//   const res = await axios.get(`https://openapi.naver.com/v1/search/local.xml?query=${search}&display=10&start=1&sort=random`, {
+//     headers: {
+//       'X-Naver-Client-Id': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_ID}`, 'X-Naver-Client-Secret': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_SECRET}`,
+//     }
+//   })
+//   return res;
+// }
