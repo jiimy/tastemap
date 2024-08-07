405441613f823d1a0f869734c381b2b9255ce1ee
rudwnok <rudwnok123@naver.com>
2024-08-07 20:37:17 +0900

    배포 실패 수정

--- src/app/api/mapSearch/route.ts
+++ src/app/api/mapSearch/route.ts
@@ -7,11 +7,12 @@ type mapType = {
 
 // 지도 검색
 export async function getMapApi({ search }: mapType) {
-  const res = await axios.get(`https://openapi.naver.com/v1/search/local.xml?query=${search}&display=10&start=1&sort=random`, {
-    headers: {
-      'X-Naver-Client-Id': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_ID}`, 'X-Naver-Client-Secret': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_SECRET}`,
-    }
-  })
-  return res;
+
+  // const res = await axios.get(`https://openapi.naver.com/v1/search/local.xml?query=${search}&display=10&start=1&sort=random`, {
+  //   headers: {
+  //     'X-Naver-Client-Id': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_ID}`, 'X-Naver-Client-Secret': `${process.env.NEXT_PUBLIC_SEARCH_CLIENT_SECRET}`,
+  //   }
+  // })
+  // return res;
 
 }
