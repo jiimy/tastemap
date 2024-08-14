bbdac8ebf290451f33bd20adcc5a25f0bed63310
rudwnok <rudwnok123@naver.com>
2024-08-14 18:18:04 +0900

    서버 액션 테스트..

--- src/app/api/board/read/route.ts
+++ src/app/api/board/read/route.ts
@@ -4,12 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE board (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(10) NOT NULL,
-    content VARCHAR(500) NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);`;
+      await sql`select * from board`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
