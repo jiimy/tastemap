01e44a8a770c2b97497a396a358d5c7badab79be
rudwnok <rudwnok123@naver.com>
2024-09-04 22:06:11 +0900

    suspense 추가, image 추가

--- src/app/(isBottomMenu)/result/search/page.tsx
+++ src/app/(isBottomMenu)/result/search/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import { useParams, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 const Search = () => {
   const searchParams = useSearchParams();
@@ -12,21 +12,26 @@ const Search = () => {
   // 카테고리 선택, 검색창등 검색했을때 결과 보여주는 페이지
   useEffect(() => {
     axios.get(`/api/map?query=${keyword}`).then((res) => {
-      const a = res.data?.cleanData?.items;
-      console.log('클라이언트', a)
-      setData(a);
-    });
+      setData(res.data?.cleanData?.items);
+    })
+      .then(() => {
+        axios.get(`/api/mapImage?query=${keyword}`).then((res) => {
+          console.log('이미지검색', res.data)
+        })
+      })
   }, [])
 
   return (
-    <div>
-      검색 페이지 {searchParams}
-      <ul>
-        {data?.map((item: any, index) => (
-          <li key={index}>{item?.title}</li>
-        ))}
-      </ul>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        검색 페이지 {searchParams}
+        <ul>
+          {data?.map((item: any, index) => (
+            <li key={index}>{item?.title}</li>
+          ))}
+        </ul>
+      </div>
+    </Suspense>
   );
 };
 
---
+++ src/app/api/mapImage/route.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const search = request.nextUrl.searchParams.get("query");
+
+  const response = await axios.get(
+    `https://openapi.naver.com/v1/search/image`,
+    {
+      params: {
+        query: search,
+      },
+      headers: {
+        "X-Naver-Client-Id": "yDj1zz6s7PIruaIuI3kU",
+        "X-Naver-Client-Secret": "7a4xvqlIgm",
+      },
+    }
+  );
+
+  const cleanData = JSON.parse(JSON.stringify(response.data));
+  return NextResponse.json({
+    cleanData,
+    // response
+  });
+}
